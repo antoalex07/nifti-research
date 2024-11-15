6b6f57c6cb4d067c3d4a4df559f31c87cba83848
antoalex07 <antoalex710@gmail.com>
2024-11-15 16:28:03 +0530

    to work on later

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import MarchingCubes from './MarchingCubes.jsx'
 // import Combine from './Combine.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <MarchingCubes />
   </StrictMode>,
 )
