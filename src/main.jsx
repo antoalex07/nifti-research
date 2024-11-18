531017b61c9acce156f2d6fa2633f2af249a4014
antoalex07 <antoalex710@gmail.com>
2024-11-18 17:54:06 +0530

    working on marching cubes algorithm

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import MarchingCubes from './MarchingCubes.jsx'
+import VolumeRenderer from './VolumeRenderer.jsx'
 // import Combine from './Combine.jsx'
 
 createRoot(document.getElementById('root')).render(
